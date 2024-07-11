ea0c1269805dbffdd7857cbd4bca4d77f0b88464
Eye-Mans <laluiman468@gmail.com>
2024-07-11 09:12:02 +0800

    add scrollreveal

---
+++ script.js
@@ -0,0 +1,20 @@
+// navbar closing when press
+const closeLinks = document.querySelectorAll('.close-nav');
+
+closeLinks.forEach(link => {
+    link.addEventListener('click', () => {
+    const navbar = document.getElementById('navbar-default');
+    navbar.classList.add('hidden');
+    });
+});
+
+// scroll reveal
+ScrollReveal({ 
+    //reset: true,
+    distance:'70px',
+    duration:1500,
+    delay:50
+ });
+
+ ScrollReveal().reveal('.heroimg, .line, .grid1, .grid2, .grid3, .picture1, .picture2, .picture3, .picture4, .picture5' , { origin: 'bottom'});
+ ScrollReveal().reveal('.aboutimg' , { origin: 'right'});
