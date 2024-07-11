adef8125d8eb51f4efe50e3a572756e1c6f70123
Eye-Mans <laluiman468@gmail.com>
2024-07-11 12:43:14 +0800

    fix the js

--- script.js
+++ script.js
@@ -37,31 +37,33 @@ if (((e.which == 85) || (e.which == 65) || (e.which == 88) || (e.which == 67) ||
 
 return false;
 }
-}
-// right click code
-var isNS = (navigator.appName == "Netscape") ? 1 : 0;
-if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
-function mischandler(){
+
+//  make the website cant right clicked
+// }
+// // right click code
+// var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+// if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+// function mischandler(){
     
-return false;
-}
-function mousehandler(e){
-var myevent = (isNS) ? e : event;
-var eventbutton = (isNS) ? myevent.which : myevent.button;
-if((eventbutton==2)||(eventbutton==3)) return false;
-}
-document.oncontextmenu = mischandler;
-document.onmousedown = mousehandler;
-document.onmouseup = mousehandler;
-//select content code disable  alok goyal
-function killCopy(e){
-return false
-}
-function reEnable(){
-return true
-}
-document.onselectstart=new Function ("return false")
-if (window.sidebar){
-document.onmousedown=killCopy
-document.onclick=reEnable
+// return false;
+// }
+// function mousehandler(e){
+// var myevent = (isNS) ? e : event;
+// var eventbutton = (isNS) ? myevent.which : myevent.button;
+// if((eventbutton==2)||(eventbutton==3)) return false;
+// }
+// document.oncontextmenu = mischandler;
+// document.onmousedown = mousehandler;
+// document.onmouseup = mousehandler;
+// //select content code disable  alok goyal
+// function killCopy(e){
+// return false
+// }
+// function reEnable(){
+// return true
+// }
+// document.onselectstart=new Function ("return false")
+// if (window.sidebar){
+// document.onmousedown=killCopy
+// document.onclick=reEnable
 }
